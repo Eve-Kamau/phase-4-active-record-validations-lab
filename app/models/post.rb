a883e0645e9f8a4ac8848ea2218cb05913df8136
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-20 22:45:41 +0300

    commit tree 1078d11fd0e9a6997b992a316fe616d97af9db8d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction]}
+    validate :title_to_be_true_facts
+
+    def title_to_be_true_facts
+        if title && !title.include?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+           errors.add(:title, "Title must be clickbaitable")
+        end
+    end
 end
+
+
